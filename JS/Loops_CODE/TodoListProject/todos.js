a336950a80b52cc8ab558361baef70f57990b51e
Jucavi <jcarvil@gmail.com>
2021-12-17 22:53:07 +0100

    Add todo list

--- JS/Loops_CODE/TodoListProject/todos.js
+++ JS/Loops_CODE/TodoListProject/todos.js
@@ -1,25 +1,33 @@
-let input = prompt('what would you like to do?');
-const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
-while (input !== 'quit' && input !== 'q') {
-    if (input === 'list') {
-        console.log('*****************')
-        for (let i = 0; i < todos.length; i++) {
-            console.log(`${i}: ${todos[i]}`);
+const todoList = ['Collect Chicken Eggs', 'Clear Litter Box'];
+let user = ''
+
+while (user !== 'quit' && user !=='q') {
+    user = prompt('What would you want to do')
+    if (user) {
+        user = user.trim().toLowerCase();
+
+        if (user === 'new') {
+            let newTodo = prompt('Enter new todo');
+            todoList.push(newTodo);
+            console.log(`${newTodo} added to list`);
         }
-        console.log('*****************')
-    } else if (input === 'new') {
-        const newTodo = prompt('Ok, what is the new todo?');
-        todos.push(newTodo);
-        console.log(`${newTodo} added to the list!`)
-    } else if (input === 'delete') {
-        const index = parseInt(prompt('Ok, enter an index to delete:'));
-        if (!Number.isNaN(index)) {
-            const deleted = todos.splice(index, 1);
-            console.log(`Ok, deleted ${deleted[0]}`);
-        } else {
-            console.log('Unknown index')
+        else if (user === 'delete') {
+            let index = parseInt(prompt('Ok, enter the index to delete'));
+            if (!Number.isNaN(index)) {
+                todoDeleted = todoList.splice(index, 1);
+                console.log(`Todo removed: ${todoDeleted}`);
+            }
+            else {
+                console.log('Unknown index')
+            }
+        }
+        else if (user === 'list') {
+            console.log('*************');
+            for (let i = 0; i < todoList.length; i++) {
+                console.log(`${i}: ${todoList[i]}`);
+            }
+            console.log('*************');
         }
     }
-    input = prompt('what would you like to do?')
 }
-console.log('OK QUIT THE APP!')
+console.log('OK, YOU QUIT THE APP');
